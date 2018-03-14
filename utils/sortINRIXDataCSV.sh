3995b9fb289b4a2f3eb9f6c5e15d4abef3df3da6
Paul Tomchik <pauljtomchik@live.com>
2018-03-13 21:19:07 -0400

    minor changes to sortINRIXDataCSV

--- utils/sortINRIXDataCSV.sh
+++ utils/sortINRIXDataCSV.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-
-INF_PATH=../data/test.csv
+INF_PATH=$1
 OUTF_PATH="${INF_PATH%csv}sorted.csv"
 
-if [ ! -f $INF_PATH ]; then
+if [ ! -f "$1" ]; then
   echo "USAGE: Specify the input csv file path with INF_PATH env variable."
   exit 1
 fi;
 
-if [ -f $OUTF_PATH ]; then
+if [ -f "$OUTF_PATH" ]; then
   echo "The output file ${OUTF_PATH} already exists."
   exit 1
 fi;
 
 function getColNum {
-  awk -F',' -v col="$1" 'NR==1{for (i=1; i<=NF; i++) if ($i==col) {print i;exit}}' $INF_PATH
+  awk -F',' -v col="$1" 'NR==1{for (i=1; i<=NF; i++) if ($i==col) {print i;exit}}' "$INF_PATH"
 }
 
 TMC_CODE_COL_NUM="$(getColNum tmc_code)"
@@ -24,12 +22,12 @@ TIMESTAMP_COL_NUM="$(getColNum measurement_tstamp)"
 DATASOURCE_COL_NUM="$(getColNum datasource)"
 
 # Preserve the header
-head -1 $INF_PATH > $OUTF_PATH
+head -1 "$INF_PATH" > "$OUTF_PATH"
 
 # Sort the data
-tail -n +2 $INF_PATH | \
+tail -n +2 "$INF_PATH" | \
   LC_ALL=C sort \
   -k"${TMC_CODE_COL_NUM},${TMC_CODE_COL_NUM}"\
   -k"${TIMESTAMP_COL_NUM},${TIMESTAMP_COL_NUM}"\
   -k"${DATASOURCE_COL_NUM},${DATASOURCE_COL_NUM}"\
-  -t',' >> ${OUTF_PATH}
+  -t',' >> "$OUTF_PATH"
