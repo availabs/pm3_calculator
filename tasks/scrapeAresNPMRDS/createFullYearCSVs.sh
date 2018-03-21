602061f7409f108db9c637ace1aca2545a4c10bd
Paul Tomchik <pauljtomchik@live.com>
2018-03-21 08:57:54 -0400

    create full year CSVs from the scraped monthly CSVs.

---
+++ tasks/scrapeAresNPMRDS/createFullYearCSVs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+cd "$( dirname "$0" )"
+
+if [ -z "$STATE" ]; then
+  echo 'USAGE: specify the STATE by env variable.'
+  exit 1
+fi
+
+export STATE
+
+YEARS=$(ls -l "../../etl/${STATE}" | awk '{print substr($9, 4, 4)}' | sed '/^$/d' | sort -u)
+
+for YEAR in "${YEARS[@]}"
+do
+  export YEAR
+  ../../utils/hereSchemaDataStream.bash.sh 
+done
