87533009dcfb148c5d39fdea5b3dbc664ad2b5c3
Paul Tomchik <pauljtomchik@live.com>
2018-03-25 21:46:46 -0400

    Refined ETL Pipeline.

--- bin/etlPipeline/partitionDownloadedCSVByMonth.sh
+++ bin/etlPipeline/partitionDownloadedCSVByMonth.sh
@@ -2,20 +2,33 @@
 
 set -e
 
+DOWNLOADED_ZIP_PATH="$(readlink -m "${1:-$DOWNLOADED_ZIP_PATH}")"
+
+PARTITIONED_INRIX_CSV_PATH_TEMPLATE="$(readlink -m "${2:-$PARTITIONED_INRIX_CSV_PATH_TEMPLATE}")"
+
 if [ -z "$DOWNLOADED_ZIP_PATH" ]
 then
-  echo "USAGE: Specify DOWNLOADED_ZIP_PATH as a env variable."
+  echo "USAGE: Specify DOWNLOADED_ZIP_PATH as a env variable or as the 1st cli arg."
+  exit 1
+fi
+
+if [ ! -f "$DOWNLOADED_ZIP_PATH" ]
+then
+  echo "$DOWNLOADED_ZIP_PATH does not exist"
   exit 1
 fi
 
 if [ -z "$PARTITIONED_INRIX_CSV_PATH_TEMPLATE" ]
 then
-  echo "USAGE: Specify PARTITIONED_INRIX_CSV_PATH_TEMPLATE as a env variable."
+  echo "USAGE: Specify PARTITIONED_INRIX_CSV_PATH_TEMPLATE as a env variable or as the 2nd cli arg."
   exit 1
 fi
 
-# Using default variables, the TMP_ETL_WORK_DIR
-mkdir -p "$(dirname "$PARTITIONED_INRIX_CSV_PATH_TEMPLATE")"
+pushd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null
+
+PARTITIONER_PATH="$(readlink -e '../partitionDataByMonth.awk')"
+
+popd > /dev/null
 
 # The data.zip included multiple files. We need the name of the datafile.
 # This file name varies.
@@ -29,11 +42,19 @@ DATA_FILE_NAME=$(\
   awk '{ print $NF }' \
 )
 
-pushd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null
+# Using default variables, the TMP_ETL_WORK_DIR
+DDIR="$(dirname "$PARTITIONED_INRIX_CSV_PATH_TEMPLATE")"
+
+mkdir -p "$DDIR"
+
+# Move the the output dir of the partitioned files
+#   This removes the risk of the __MONTH__ placeholder
+#   ever being part of the file's path.
+pushd "$DDIR" > /dev/null
 
 awk -F',' \
-  -v fpath_template="$PARTITIONED_INRIX_CSV_PATH_TEMPLATE" \
-  -f ../partitionDataByMonth.awk \
+  -v fpath_template="$(basename "$PARTITIONED_INRIX_CSV_PATH_TEMPLATE")" \
+  -f "$PARTITIONER_PATH" \
   <(unzip -p "$DOWNLOADED_ZIP_PATH" "$DATA_FILE_NAME")
 
 popd > /dev/null
--- bin/etlPipeline/sortInrixSchemaCSVs.sh
+++ bin/etlPipeline/sortInrixSchemaCSVs.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-ETL_WORK_DIR="${ETL_WORK_DIR:=$1}"
+ETL_WORK_DIR="$( readlink -f "${1:-ETL_WORK_DIR}" )"
 
 if [ -z "$ETL_WORK_DIR" ]
 then
@@ -11,10 +11,9 @@ then
 fi
 
 SORTER_PATH="$( dirname "${BASH_SOURCE[0]}" )/../sortINRIXDataCSV.sh"
-SORTER_PATH=$(readlink -f "$SORTER_PATH")
+SORTER_PATH=$( readlink -f "$SORTER_PATH" )
 
-ETL_WORK_DIR=$(readlink -f "${ETL_WORK_DIR}")
-
+# Change directory to the work dir
 pushd "$ETL_WORK_DIR" > /dev/null
 
 ARR=(`find . -regex ".*\.[1-2][0-9][0-1][0-9][0-9][0-9]${INRIX_SCHEMA_CSV_EXTENSION}" | sort`)
