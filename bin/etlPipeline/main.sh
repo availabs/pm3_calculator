27b368ee0e196f5dfd2f6423a006beea093d21b9
Paul Tomchik <pauljtomchik@live.com>
2018-03-25 21:46:46 -0400

    Refined ETL Pipeline.

--- bin/etlPipeline/main.sh
+++ bin/etlPipeline/main.sh
@@ -5,12 +5,18 @@
 
 set -e
 
+source "$( dirname "${BASH_SOURCE[0]}")/setDefaultVariables.sh"
+
 pushd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null
 
-source ./setDefaultVariables.sh
-
-echo 'downloadFromRITIS'
-source ./downloadFromRITIS.sh
+if [ ! -f "$DOWNLOADED_ZIP_PATH" ]
+then
+  echo 'downloadFromRITIS'
+  source ./downloadFromRITIS.sh
+else
+  echo 'Skipping downloadFromRITIS.'
+  echo "  Using existing zip archive at ${DOWNLOADED_ZIP_PATH}"
+fi
 
 echo 'partitionDownloadedCSVByMonth'
 source ./partitionDownloadedCSVByMonth.sh
