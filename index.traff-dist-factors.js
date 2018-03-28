3e0b81901ca56f49c23b3cbc3fcba80946afa272
Paul Tomchik <pauljtomchik@live.com>
2018-03-28 12:03:58 -0400

    Richer traffic dist factors calculator output.

---
+++ index.traff-dist-factors.js
@@ -0,0 +1,116 @@
+#!/usr/bin/env node
+
+const { env } = process;
+
+const Promise = require('bluebird');
+const fs = require('fs');
+
+const { split, through, stringify } = require('event-stream');
+const transform = require('parallel-transform');
+
+const minimist = require('minimist');
+const argv = minimist(process.argv.slice(2));
+
+const {
+  DownloadTMCAtttributes,
+  getTrafficDistribution
+} = require('./utils/data_retrieval');
+
+const csvInputStream = require('./utils/csvInputStream');
+const tmcAggregator = require('./utils/inrixCSVParserStream/tmcAggregator');
+const csvOutputStream = require('./utils/csvOutputStream');
+
+const CalculatePHED = require('./calculators/phed');
+const CalculateTTR = require('./calculators/ttr');
+const CalculateTrafficDistFactors = require('./calculators/trafficDistributionFactors');
+
+const outputCols = [
+  'tmc',
+  'congestion_level',
+  'directionality',
+  'combinedPeakAvgTT',
+  'amPeakAvgTT',
+  'pmPeakAvgTT',
+  'freeFlowAvgTT',
+  'speedReductionFactor',
+  'peakTimeDifferential',
+  'peakSpeedDifferential'
+];
+
+const toNumerics = o =>
+  Object.keys(o).reduce((acc, k) => {
+    acc[k] = Number.isFinite(+o[k]) ? parseFloat(o[k]) : o[k];
+    return acc;
+  }, {});
+
+const { CONCURRENCY = 4, STATE = 'nj' } = toNumerics(
+  Object.assign({}, env, argv)
+);
+
+const calculateTrafficDistributionFactors = tmcAttributes => {
+  return transform(
+    CONCURRENCY,
+    // Data schema:
+    // {
+    //    meta: {
+    //      tmc: <tmc code>
+    //      year: <year>
+    //    },
+    //
+    //    data: [
+    //      {
+    //        npmrds_date
+    //        epoch
+    //        travel_time_all_vehicles
+    //       },
+    //       ...
+    //    ]
+    function write(tmcData, callback) {
+      const { metadata: { tmc }, data } = tmcData;
+
+      const attrs = tmcAttributes[tmc];
+
+      if (!attrs) {
+        return;
+      }
+
+      return callback(
+        null,
+        Object.assign(
+          {},
+          { tmc },
+          CalculateTrafficDistFactors({
+            attrs,
+            data
+          })
+        )
+      );
+    }
+  );
+};
+
+async function doIt() {
+  const result = await DownloadTMCAtttributes(STATE);
+
+  const tmcAttributes = result.rows.reduce(
+    (acc, row) => Object.assign(acc, { [row.tmc]: row }),
+    {}
+  );
+
+  // https://stackoverflow.com/a/15884508/3970755
+  process.stdout.on('error', function(err) {
+    if (err.code == 'EPIPE') {
+      process.exit(0);
+    }
+  });
+
+  process.stdin
+    .pipe(split())
+    .pipe(csvInputStream())
+    .pipe(tmcAggregator())
+    .pipe(calculateTrafficDistributionFactors(tmcAttributes))
+    .pipe(csvOutputStream(outputCols))
+    .pipe(process.stdout);
+}
+
+doIt();
