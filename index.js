760369d8a4ba3499f4d8e45a41871cd378e10183
avail <avail@avail-lor.ba.albany.edu>
2018-03-29 23:24:04 -0400

    changed index

--- index.js
+++ index.js
@@ -63,7 +63,9 @@ const CalculateMeasures = function CalculateMeasures(tmc, year) {
         }
         return output;
       }, {});
-
+      if ( Object.keys(tmcFiveteenMinIndex).length < 1 ){
+	resolve(null); return;
+      }
       var phed = CalculatePHED(
         tmc,
         tmcFiveteenMinIndex,
