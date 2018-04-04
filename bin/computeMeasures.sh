79b45fa5d565ca4a57001fda7013ffc43c00020c
dvadney <dvad924@gmail.com>
2018-04-04 11:57:52 -0400

    added full to compute script

--- bin/computeMeasures.sh
+++ bin/computeMeasures.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-YEAR=2015 STATE=ny node --harmony index.js
-YEAR=2016 STATE=ny node --harmony index.js
-YEAR=2017 STATE=ny node --harmony index.js
+FULL=true YEAR=2015 STATE=ny node --harmony index.js
+FULL=true YEAR=2016 STATE=ny node --harmony index.js
+FULL=true YEAR=2017 STATE=ny node --harmony index.js
