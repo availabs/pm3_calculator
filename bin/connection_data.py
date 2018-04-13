98f151088c0ba0da357d4a7cd8a6512d4ed1e65d
avail <avail@avail-lor.ba.albany.edu>
2018-04-13 08:22:25 -0400

    test 123

---
+++ bin/connection_data.py
@@ -0,0 +1,4 @@
+ConnectionData={"database":'npmrds_test',
+                      "user":'npmrds_ninja',
+                      "password":'***REMOVED***',
+                      "host":'ares.availabs.org'}
