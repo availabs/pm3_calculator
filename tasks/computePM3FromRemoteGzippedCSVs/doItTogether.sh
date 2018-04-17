2fab63ae2dccb77864f7f319e456ab398a67e449
avail <avail@vulcan.BA.ALBANY.EDU>
2018-04-17 08:44:15 -0400

    computePM3FromRemoteGzippedCSVs

--- tasks/computePM3FromRemoteGzippedCSVs/doItTogether.sh
+++ tasks/computePM3FromRemoteGzippedCSVs/doItTogether.sh
@@ -9,9 +9,11 @@ pushd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null
 
 COUNTER=0
 
+../../bin/getPM3ColumnsFromDB.sh > ../../utils/pm3OutputCols.json
+
 while [  $COUNTER -lt "$CONCURRENCY" ]; do
-	./doIt.sh &
-	let COUNTER=COUNTER+1 
+  ./doIt.sh &
+  let COUNTER=COUNTER+1 
 done
 
 wait
