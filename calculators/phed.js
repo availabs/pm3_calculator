8418a1e3b3927d8f319ad44fc0f22fd260f8f429
Paul Tomchik <pauljtomchik@live.com>
2018-05-23 15:01:25 -0400

    Simple refactoring of phed & ttr calculators. Output unchanged.

--- calculators/phed.js
+++ calculators/phed.js
@@ -1,17 +1,26 @@
+/* eslint camelcase: 0 */
+/* eslint no-use-before-define: 0 */
+/* eslint prefer-destructuring: 0 */
+/* eslint no-unused-vars: 0 */
+/* eslint no-param-reassign: 0 */
+/* eslint no-return-assign: 0 */
+/* eslint no-restricted-properties: 0 */
+
 const dayVolume = [0.8, 1.05, 1.05, 1.05, 1.05, 1.1, 0.9];
-const { getAadt } = require("./utils/aadtUtils");
-const { getTT } = require("./utils/indexutils");
+const { getAadt } = require('./utils/aadtUtils');
+const { getTT } = require('./utils/indexutils');
+
 const calculatePHED = function calculatePHED(
   tmcAttributes,
   tmcFiveteenMinIndex,
   distroArray,
   time = 12,
-  mean = "hmean"
+  mean = 'hmean'
 ) {
-  let ttypes = ["", "singl", "combi", "truck", "pass"];
-  let data = {};
+  const ttypes = ['', 'singl', 'combi', 'truck', 'pass'];
+  const data = {};
   ttypes.forEach(tt => {
-    let {
+    const {
       vehicle_delay,
       delay,
       vehicle_delay_all,
@@ -33,96 +42,91 @@ const calculateAllPHED = function calculateAllPHED(
   tmcFiveteenMinIndex,
   distroArray,
   time = 12,
-  mean = "hmean",
-  trafficType = ""
+  mean = 'hmean',
+  trafficType = ''
 ) {
-  let ttlabel = trafficType.length ? `_${trafficType}` : "";
-  var tmc = tmcAttributes.tmc;
-  var dirFactor = +tmcAttributes.faciltype > 1 ? 2 : 1;
-  var DirectionalAADT = getAadt(tmcAttributes, trafficType) / dirFactor;
+  const ttlabel = trafficType.length ? `_${trafficType}` : '';
+  const tmc = tmcAttributes.tmc;
+  const dirFactor = +tmcAttributes.faciltype > 1 ? 2 : 1;
+  const DirectionalAADT = getAadt(tmcAttributes, trafficType) / dirFactor;
 
-  var distroData = distroArray.map((val, index) => {
-    return { hour: index, count: val };
-  });
-  //console.log('distro data', distroArray.reduce((a,b) => a + b ))
-  var TestVolume = distroArray.map(d => {
-    return d * DirectionalAADT;
-  });
-  //console.log('TestVolume', TestVolume.reduce((a,b) => a + b ), DirectionalAADT)
+  const distroData = distroArray.map((val, index) => ({
+    hour: index,
+    count: val
+  }));
+  // console.log('distro data', distroArray.reduce((a,b) => a + b ))
+  const TestVolume = distroArray.map(d => d * DirectionalAADT);
+  // console.log('TestVolume', TestVolume.reduce((a,b) => a + b ), DirectionalAADT)
 
-  //tmcAttributes.avg_speedlimit = 45 // this is supid
-  var ThresholdSpeed =
+  // tmcAttributes.avg_speedlimit = 45 // this is supid
+  const ThresholdSpeed =
     +tmcAttributes.avg_speedlimit * 0.6 > 20
       ? +tmcAttributes.avg_speedlimit * 0.6
       : 20;
 
-  var fifteenData = Object.keys(tmcFiveteenMinIndex).map((key, i) => {
-    var epoch = key.split("_")[1];
-    var hour = Math.floor(epoch / 4).toString();
-    hour = hour.length === 1 ? "0" + hour : hour;
-    var min = ((epoch % 4) * 15).toString();
-    min = min.length === 1 ? "0" + min : min;
-    var dateString = key.split("_")[0];
-    var yearMonthDay =
-      dateString.substring(0, 4) +
-      "-" +
-      dateString.substring(4, 6) +
-      "-" +
-      dateString.substring(6, 8);
-    var dateTime = new Date(yearMonthDay + "T" + hour + ":" + min + ":00");
-    var len = tmcFiveteenMinIndex[key].speed.length;
+  const fifteenData = Object.keys(tmcFiveteenMinIndex).map((key, i) => {
+    const epoch = key.split('_')[1];
+    let hour = Math.floor(epoch / 4).toString();
+    hour = hour.length === 1 ? `0${hour}` : hour;
+    let min = ((epoch % 4) * 15).toString();
+    min = min.length === 1 ? `0${min}` : min;
+    const dateString = key.split('_')[0];
+    const yearMonthDay = `${dateString.substring(0, 4)}-${dateString.substring(
+      4,
+      6
+    )}-${dateString.substring(6, 8)}`;
+    const dateTime = new Date(`${yearMonthDay}T${hour}:${min}:00`);
+    const len = tmcFiveteenMinIndex[key].speed.length;
     // var sum_speed = tmcFiveteenMinIndex[key].speed.reduce((a, b) => (a += b));
     // var hsum_speed = tmcFiveteenMinIndex[key].speed.reduce((a, b) => {
     //   return (a += 1 / b);
     // }, 0);
     // var hmean_speed = precisionRound(len / hsum_speed, 2);
     // var mean_speed = precisionRound(sum_speed / len, 2);
-    var sum_tt = getTT(tmcFiveteenMinIndex, key, trafficType).reduce(
+    const sum_tt = getTT(tmcFiveteenMinIndex, key, trafficType).reduce(
       (a, b) => (a += b)
     );
-    var hsum_tt = getTT(tmcFiveteenMinIndex, key, trafficType).reduce(
-      (a, b) => {
-        return (a += 1 / b);
-      },
+    const hsum_tt = getTT(tmcFiveteenMinIndex, key, trafficType).reduce(
+      (a, b) => (a += 1 / b),
       0
     );
-    var hmean_tt = precisionRound(len / hsum_tt, 0);
-    var mean_tt = precisionRound(sum_tt / len, 0);
-    var threshold_travelTime = Math.round(
+    const hmean_tt = precisionRound(len / hsum_tt, 0);
+    const mean_tt = precisionRound(sum_tt / len, 0);
+    const threshold_travelTime = Math.round(
       tmcAttributes.length / ThresholdSpeed * 3600
     );
-    var speedlimit_travelTime =
+    const speedlimit_travelTime =
       tmcAttributes.length / +tmcAttributes.avg_speedlimit * 3600;
-    var hmean_delay =
+    let hmean_delay =
       hmean_tt > threshold_travelTime
         ? Math.min(Math.round(hmean_tt) - threshold_travelTime, 900) / 3600
         : null;
-    var mean_delay =
+    let mean_delay =
       mean_tt > threshold_travelTime
         ? Math.min(Math.round(mean_tt) - threshold_travelTime, 900) / 3600
         : null;
     hmean_delay = precisionRound(hmean_delay, 4);
     mean_delay = precisionRound(mean_delay, 4);
-    var days = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"];
+    const days = ['Sun', 'Mon', 'Tues', 'Weds', 'Thurs', 'Fri', 'Sat'];
 
     // Calculate Daily AADT then Disagregate
     // var dailyAADT = dayVolume[dateTime.getDay()] * DirectionalAADT
     // var TrafficVolume = distroArray.map(d => {return (d / 100) * dailyAADT})
 
-    var dailyAADT = DirectionalAADT;
-    var TrafficVolume = distroArray.map(d => {
-      return d * dailyAADT * dayVolume[dateTime.getDay()];
-    });
+    const dailyAADT = DirectionalAADT;
+    const TrafficVolume = distroArray.map(
+      d => d * dailyAADT * dayVolume[dateTime.getDay()]
+    );
 
-    var fifteenMinuteVolumes =
+    const fifteenMinuteVolumes =
       time === 12
         ? precisionRound(+TrafficVolume[parseInt(hour)] / 4, 1)
         : precisionRound(+TrafficVolume[epoch], 1);
 
     // console.log('Traffic Volume', TrafficVolume)
     // console.log('fifteen Minute Volumes', fifteenMinuteVolumes)
-    var hmean_vehicle_delay = hmean_delay * fifteenMinuteVolumes;
-    var mean_vehicle_delay = mean_delay * fifteenMinuteVolumes;
+    let hmean_vehicle_delay = hmean_delay * fifteenMinuteVolumes;
+    let mean_vehicle_delay = mean_delay * fifteenMinuteVolumes;
     hmean_vehicle_delay = precisionRound(hmean_vehicle_delay, 3);
     mean_vehicle_delay = precisionRound(mean_vehicle_delay, 3);
 
@@ -131,15 +135,15 @@ const calculateAllPHED = function calculateAllPHED(
       dateTime,
       epoch,
       speedlimit: +tmcAttributes.avg_speedlimit,
-      tt: mean === "hmean" ? hmean_tt : mean_tt,
-      delay: mean === "hmean" ? hmean_delay : mean_delay,
+      tt: mean === 'hmean' ? hmean_tt : mean_tt,
+      delay: mean === 'hmean' ? hmean_delay : mean_delay,
       vehicle_delay:
-        mean === "hmean" ? hmean_vehicle_delay : hmean_vehicle_delay
+        mean === 'hmean' ? hmean_vehicle_delay : hmean_vehicle_delay
     };
   });
 
-  var fifteenPeaks = fifteenData.map(d => {
-    var out = Object.assign({}, d);
+  let fifteenPeaks = fifteenData.map(d => {
+    const out = Object.assign({}, d);
     if (!((d.epoch >= 24 && d.epoch < 40) || (d.epoch >= 60 && d.epoch < 76))) {
       out.tt = null;
       out.delay = null;
@@ -158,31 +162,29 @@ const calculateAllPHED = function calculateAllPHED(
     return out;
   });
   let fifteenTotal = fifteenData.map(x => Object.assign({}, x));
-  //let analysis_data = fifteenPeaks.filter(d => d.vehicle_delay_bp)
+  // let analysis_data = fifteenPeaks.filter(d => d.vehicle_delay_bp)
   fifteenPeaks = fifteenPeaks.filter(d => d.vehicle_delay);
   fifteenTotal = fifteenTotal.filter(d => d.vehicle_delay);
-  var delay = {};
-  var vehicle_delay = {};
-  let delay_all = {};
-  let vehicle_delay_all = {};
+  const delay = {};
+  const vehicle_delay = {};
+  const delay_all = {};
+  const vehicle_delay_all = {};
   const array_sum = (data, accessor) =>
     data.reduce((acc, c) => acc + accessor(c), 0);
 
-  var months = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].forEach(month => {
-    var raw_data = fifteenPeaks.filter(d => {
-      return d.dateTime.getMonth() === month;
-    });
-    let raw_all_data = fifteenTotal.filter(
+  const months = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].forEach(month => {
+    const raw_data = fifteenPeaks.filter(d => d.dateTime.getMonth() === month);
+    const raw_all_data = fifteenTotal.filter(
       d => d.dateTime.getMonth() === month
     );
 
-    var cur_delay = raw_data.reduce((out, curr) => {
+    const cur_delay = raw_data.reduce((out, curr) => {
       out += curr.delay;
       return out;
     }, 0);
     delay[`d${ttlabel}_${month + 1}`] = precisionRound(cur_delay, 3);
 
-    var curr_vehicle_delay = raw_data.reduce((out, curr) => {
+    const curr_vehicle_delay = raw_data.reduce((out, curr) => {
       out += curr.vehicle_delay;
       return out;
     }, 0);
@@ -191,39 +193,38 @@ const calculateAllPHED = function calculateAllPHED(
       3
     );
 
-    var cur_all_delay = array_sum(raw_all_data, x => x.delay);
-    var cur_all_vehicle_delay = array_sum(raw_all_data, x => x.vehicle_delay);
-    let key = `${month + 1}`;
+    const cur_all_delay = array_sum(raw_all_data, x => x.delay);
+    const cur_all_vehicle_delay = array_sum(raw_all_data, x => x.vehicle_delay);
+    const key = `${month + 1}`;
     delay_all[`td${ttlabel}_${key}`] = precisionRound(cur_all_delay, 3);
     vehicle_delay_all[`tvd${ttlabel}_${key}`] = precisionRound(
       cur_all_vehicle_delay,
       3
     );
   });
-  let all_vehicle_delay = precisionRound(
+  const all_vehicle_delay = precisionRound(
     array_sum(fifteenTotal, x => x.vehicle_delay),
     3
   );
-  let all_delay = precisionRound(array_sum(fifteenTotal, x => x.delay), 3);
+  const all_delay = precisionRound(array_sum(fifteenTotal, x => x.delay), 3);
   delay_all[`td${ttlabel}_total`] = all_delay;
   vehicle_delay_all[`tvd${ttlabel}_total`] = all_vehicle_delay;
-  vehicle_total = precisionRound(
+
+  vehicle_delay[`vd${ttlabel}_total`] = precisionRound(
     fifteenPeaks.reduce((out, curr) => {
       out += curr.vehicle_delay;
       return out;
     }, 0),
     3
   );
-  vehicle_delay[`vd${ttlabel}_total`] = vehicle_total;
 
-  delay_total = precisionRound(
+  delay[`d${ttlabel}_total`] = precisionRound(
     fifteenPeaks.reduce((out, curr) => {
       out += curr.delay;
       return out;
     }, 0),
     3
   );
-  delay[`d${ttlabel}_total`] = delay_total;
 
   // console.log(delay, vehicle_delay)
 
@@ -238,6 +239,6 @@ const calculateAllPHED = function calculateAllPHED(
 module.exports = calculatePHED;
 
 function precisionRound(number, precision) {
-  var factor = Math.pow(10, precision);
+  const factor = Math.pow(10, precision);
   return Math.round(number * factor) / factor;
 }
--- calculators/ttr.js
+++ calculators/ttr.js
@@ -1,35 +1,44 @@
-let d3 = require("d3-array");
-const precisionRound = require("./utils/precisionRound");
+/* eslint no-unused-vars: 0 */
+/* eslint camelcase: 0 */
+/* eslint no-return-assign: 0 */
+/* eslint no-param-reassign: 0 */
+/* eslint no-shadow: 0 */
+
+const d3 = require('d3-array');
+const precisionRound = require('./utils/precisionRound');
+
 const WEEKDAYS = [1, 2, 3, 4, 5];
 const WEEKENDS = [0, 6];
 
 const CalculateTTR = function CalculateLottr(
   tmc,
   tmcFiveteenMinIndex,
-  mean = "mean"
+  mean = 'mean'
 ) {
-  let months = {};
-  var fifteenData = Object.keys(tmcFiveteenMinIndex).map((key, i) => {
-    var epoch = key.split("_")[1];
-    var hour = Math.floor(epoch / 4).toString();
-    hour = hour.length === 1 ? "0" + hour : hour;
-    var min = ((epoch % 4) * 15).toString();
-    min = min.length === 1 ? "0" + min : min;
-    var dateString = key.split("_")[0];
-    var year = dateString.substring(0, 4);
-    var month = dateString.substring(4, 6);
-    var day = dateString.substring(6, 8);
-    var yearMonthDay = year + "-" + month + "-" + day;
-    var dateTime = new Date(yearMonthDay + "T" + hour + ":" + min + ":00");
+  const months = {};
+  const fifteenData = Object.keys(tmcFiveteenMinIndex).map((key, i) => {
+    const epoch = key.split('_')[1];
+    let hour = Math.floor(epoch / 4).toString();
+    hour = hour.length === 1 ? `0${hour}` : hour;
+    let min = ((epoch % 4) * 15).toString();
+    min = min.length === 1 ? `0${min}` : min;
+    const dateString = key.split('_')[0];
+    const year = dateString.substring(0, 4);
+    const month = dateString.substring(4, 6);
+    const day = dateString.substring(6, 8);
+    const yearMonthDay = `${year}-${month}-${day}`;
+    const dateTime = new Date(`${yearMonthDay}T${hour}:${min}:00`);
     months[dateTime.getMonth()] = true;
-    var sum_tt = tmcFiveteenMinIndex[key].tt.reduce((a, b) => (a += b));
-    var hsum_tt = tmcFiveteenMinIndex[key].tt.reduce((a, b) => {
-      return (a += 1 / b);
-    }, 0);
-    var len = tmcFiveteenMinIndex[key].tt.length;
-    var hmean = hsum_tt;
-    var mean = sum_tt;
-    mean = sum_tt / len
+
+    const sum_tt = tmcFiveteenMinIndex[key].tt.reduce((a, b) => (a += b));
+    const hsum_tt = tmcFiveteenMinIndex[key].tt.reduce(
+      (a, b) => (a += 1 / b),
+      0
+    );
+    const len = tmcFiveteenMinIndex[key].tt.length;
+    const hmean = hsum_tt;
+    let mean = sum_tt;
+    mean = sum_tt / len;
 
     return {
       dateTime,
@@ -38,8 +47,8 @@ const CalculateTTR = function CalculateLottr(
       mean
     };
   });
-  let monthvals = Object.keys(months).map(x => +x);
-  monthvals.push("total");
+  const monthvals = Object.keys(months).map(x => +x);
+  monthvals.push('total');
   function numSort(a, b) {
     return +a - +b;
   }
@@ -48,82 +57,77 @@ const CalculateTTR = function CalculateLottr(
   const tttr = scores =>
     precisionRound(d3.quantile(scores, 0.95) / d3.quantile(scores, 0.5), 2);
 
-  let measures = monthvals.reduce((acc, m) => {
-    let month_clause =
-      m !== "total" ? d => d.dateTime.getMonth() === m : d => true;
-    var amPeak = fifteenData
-      .filter(d => {
-        return (
+  const measures = monthvals.reduce((acc, m) => {
+    const month_clause =
+      m !== 'total' ? d => d.dateTime.getMonth() === m : d => true;
+    const amPeak = fifteenData
+      .filter(
+        d =>
           WEEKDAYS.includes(d.dateTime.getDay()) &&
           (d.epoch >= 24 && d.epoch < 40) &&
           month_clause(d)
-        );
-      })
+      )
       .map(d => d[mean])
       .sort(numSort);
 
-    var offPeak = fifteenData
-      .filter(d => {
-        return (
+    const offPeak = fifteenData
+      .filter(
+        d =>
           WEEKDAYS.includes(d.dateTime.getDay()) &&
           (d.epoch >= 40 && d.epoch < 64) &&
           month_clause(d)
-        );
-      })
+      )
       .map(d => d[mean])
       .sort(numSort);
 
-    var pmPeak = fifteenData
-      .filter(d => {
-        return (
+    const pmPeak = fifteenData
+      .filter(
+        d =>
           WEEKDAYS.includes(d.dateTime.getDay()) &&
           (d.epoch >= 64 && d.epoch < 80) &&
           month_clause(d)
-        );
-      })
+      )
       .map(d => d[mean])
       .sort(numSort);
 
-    var weekendPeak = fifteenData
-      .filter(d => {
-        return (
+    const weekendPeak = fifteenData
+      .filter(
+        d =>
           WEEKENDS.includes(d.dateTime.getDay()) &&
           (d.epoch >= 24 && d.epoch < 80) &&
           month_clause(d)
-        );
-      })
+      )
       .map(d => d[mean])
       .sort(numSort);
 
-    var overnightPeak = fifteenData
-      .filter(d => {
-        return (
+    const overnightPeak = fifteenData
+      .filter(
+        d =>
           WEEKDAYS.includes(d.dateTime.getDay()) &&
           (d.epoch < 24 || d.epoch > 80) &&
           month_clause(d)
-        );
-      })
+      )
       .map(d => d[mean])
       .sort(numSort);
-    acc["lottr"] = acc["lottr"] || {};
-    acc["tttr"] = acc["tttr"] || {};
-    let month_key = m === "total" ? "" : `_${m + 1}`;
-    acc["lottr"][`lottr_am${month_key}`] = lottr(amPeak);
-    acc["lottr"][`lottr_off${month_key}`] = lottr(offPeak);
-    acc["lottr"][`lottr_pm${month_key}`] = lottr(pmPeak);
-    acc["lottr"][`lottr_weekend${month_key}`] = lottr(weekendPeak);
+    acc.lottr = acc.lottr || {};
+    acc.tttr = acc.tttr || {};
+    const month_key = m === 'total' ? '' : `_${m + 1}`;
+    acc.lottr[`lottr_am${month_key}`] = lottr(amPeak);
+    acc.lottr[`lottr_off${month_key}`] = lottr(offPeak);
+    acc.lottr[`lottr_pm${month_key}`] = lottr(pmPeak);
+    acc.lottr[`lottr_weekend${month_key}`] = lottr(weekendPeak);
 
-    acc["tttr"][`tttr_am${month_key}`] = tttr(amPeak);
-    acc["tttr"][`tttr_off${month_key}`] = tttr(offPeak);
-    acc["tttr"][`tttr_pm${month_key}`] = tttr(pmPeak);
-    acc["tttr"][`tttr_weekend${month_key}`] = tttr(weekendPeak);
-    acc["tttr"][`tttr_overnight${month_key}`] = tttr(overnightPeak);
+    acc.tttr[`tttr_am${month_key}`] = tttr(amPeak);
+    acc.tttr[`tttr_off${month_key}`] = tttr(offPeak);
+    acc.tttr[`tttr_pm${month_key}`] = tttr(pmPeak);
+    acc.tttr[`tttr_weekend${month_key}`] = tttr(weekendPeak);
+    acc.tttr[`tttr_overnight${month_key}`] = tttr(overnightPeak);
 
     return acc;
   }, {});
-  //console.log('overnightPeak')
-  //console.log(overnightPeak)
-  //console.log(d3.quantile(overnightPeak, 0.95 ), d3.quantile(overnightPeak, 0.5),d3.quantile(overnightPeak, 0.8 ) / d3.quantile(overnightPeak, 0.5))
+  // console.log('overnightPeak')
+  // console.log(overnightPeak)
+  // console.log(d3.quantile(overnightPeak, 0.95 ), d3.quantile(overnightPeak, 0.5),d3.quantile(overnightPeak, 0.8 ) / d3.quantile(overnightPeak, 0.5))
   // var someData = fifteenData
   // 	.map(d => d[mean])
   // 	.sort()
