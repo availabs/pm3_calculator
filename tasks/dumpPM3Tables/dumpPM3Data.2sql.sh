8eaff1daa28b02307d992d2e8ec0583d3fcfb416
Paul Tomchik <pauljtomchik@live.com>
2018-05-29 10:27:10 -0400

    removed phantom tasks/dumpPM3Tables

--- tasks/dumpPM3Tables/dumpPM3Data.2sql.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-set -a
-
-source ../../config/postgres.env
-
-pg_dump --verbose --no-owner --clean --if-exists --table='*.pm3*' --exclude-table='*test*' | GZIP=-9 gzip > ./pm3.dump.sql.gz
--- tasks/dumpPM3Tables/restoreTables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-set -a
-
-source ../../config/postgres.env
-
-psql -v ON_ERROR_STOP=1 -f ./schema-only.dump.sql
-psql -v ON_ERROR_STOP=1 -f ./pm3.dump.sql
