e8684bcab10b125e6ac02060561af6f6aebba68d
paultomchik <pauljtomchik@gmail.com>
2018-09-24 18:31:48 -0400

    Speed-based and Speed-hmean-based freeflow

--- src/calculators/freeflow.js
+++ src/calculators/freeflow.js
@@ -1,58 +1,97 @@
-  // AMpeak: 6-9
-  // PMpeak: 4-7
-const percentile = require("percentile");
-const concat = require("./utils/concat");
+// AMpeak: 6-9
+// PMpeak: 4-7
+const percentile = require('percentile');
+const concat = require('./utils/concat');
+const precisionRound = require('./utils/precisionRound');
 
 const buildDateTime = key => {
-  var epoch = key.split("_")[1];
-  var hour = Math.floor(epoch / 4).toString();
-  hour = hour.length === 1 ? "0" + hour : hour;
-  var min = ((epoch % 4) * 15).toString();
-  min = min.length === 1 ? "0" + min : min;
-  var dateString = key.split("_")[0];
-  var yearMonthDay =
-    dateString.substring(0, 4) +
-    "-" +
-    dateString.substring(4, 6) +
-    "-" +
-    dateString.substring(6, 8);
-  var dateTime = new Date(yearMonthDay + "T" + hour + ":" + min + ":00");
+  const epoch = key.split('_')[1];
+  let hour = Math.floor(epoch / 4).toString();
+  hour = hour.length === 1 ? `0${hour}` : hour;
+  let min = ((epoch % 4) * 15).toString();
+  min = min.length === 1 ? `0${min}` : min;
+  const dateString = key.split('_')[0];
+  const yearMonthDay = `${dateString.substring(0, 4)}-${dateString.substring(
+    4,
+    6
+  )}-${dateString.substring(6, 8)}`;
+  const dateTime = new Date(`${yearMonthDay}T${hour}:${min}:00`);
   return dateTime;
 };
 
 const getDay = dt => dt.getDay();
 const getTime = dt => dt.getHours();
 
-const validDayTime = (d, t) =>
-  (d > 0 && d <= 5 && ((t >= 9 && t < 16) || (t >= 19 && t < 22))) ||
-  ((d === 0 || d === 6) && (t >= 6 && t < 22));
+const validDayTime = (dow, hr) =>
+  (dow > 0 && dow <= 5 && ((hr >= 9 && hr < 16) || (hr >= 19 && hr < 22))) ||
+  ((dow === 0 || dow === 6) && (hr >= 6 && hr < 22));
 
 const validTime = datetime => validDayTime(getDay(datetime), getTime(datetime));
 
-const CalculateFreeFlow = (tmcAtts, tmcFifteenMinIndex) => {
-  let TTs = Object.keys(tmcFifteenMinIndex);
-  let totalTTs = TTs.reduce((acc, k) => {
-    let tts = tmcFifteenMinIndex[k].tt;
-    concat(acc, tts);
-    return acc;
-  }, []);
-  let offPeakTTs = TTs.filter(k => {
-    let datetime = buildDateTime(k);
-    return validTime(datetime);
-  }).reduce((acc, k) => {
-    let tts = tmcFifteenMinIndex[k].tt;
+const CalculateFreeFlow = (tmcAtts = {}, tmcFifteenMinIndex) => {
+  const { length } = tmcAtts;
+
+  const TTs = Object.keys(tmcFifteenMinIndex);
+
+  const totalTTs = TTs.reduce((acc, k) => {
+    const tts = tmcFifteenMinIndex[k].tt;
     concat(acc, tts);
     return acc;
   }, []);
 
-  // let freeflowTT = percentile(30, totalTTs);
-  let freeflowUTT = percentile(30, totalTTs);
-  // let freeflowUTT = percentile(15, offPeakTTs);
-  let freeflowTT = percentile(15, offPeakTTs);
+  const offPeakTTs = TTs.filter(k => {
+    const datetime = buildDateTime(k);
+    return validTime(datetime);
+  }).reduce((acc, k) => {
+    const tts = tmcFifteenMinIndex[k].tt;
+    concat(acc, tts);
+    return acc;
+  }, []);
+
+  const offPeakSpeeds = length
+    ? TTs.filter(k => {
+        const datetime = buildDateTime(k);
+        return validTime(datetime);
+      }).reduce((acc, k) => {
+        const tts = tmcFifteenMinIndex[k].tt;
+        const speeds = tts.map(tt => precisionRound(length / tt * 3600, 1));
+
+        concat(acc, speeds);
+        return acc;
+      }, [])
+    : null;
+
+  const offPeakSpeedsHMean = length
+    ? TTs.filter(k => {
+        const datetime = buildDateTime(k);
+        return validTime(datetime);
+      }).reduce((acc, k) => {
+        const tts = tmcFifteenMinIndex[k].tt;
+        const speeds = tts.map(tt => length / tt * 3600);
+
+        const hmean = speeds.length
+          ? speeds.length / speeds.reduce((acc2, speed) => acc2 + 1 / speed, 0)
+          : null;
+
+        if (hmean) {
+          acc.push(hmean);
+        }
+        return acc;
+      }, [])
+    : null;
+
+  const freeflowUTT = percentile(30, totalTTs);
+  const freeflowTT = percentile(15, offPeakTTs);
+
+  const freeflowSpeed = offPeakSpeeds && percentile(85, offPeakSpeeds);
+  const freeflowSpeedHMean =
+    offPeakSpeedsHMean && percentile(85, offPeakSpeedsHMean);
 
   return {
     freeflowTT,
-    freeflowUTT
+    freeflowUTT,
+    freeflowSpeed,
+    freeflowSpeedHMean
   };
 };
 
