7df74fd66654bed721f03ae85ae3c6851d256d1f
paultomchik <pauljtomchik@gmail.com>
2018-09-25 14:58:00 -0400

    HPMS CSV percentile columns to 2 decimal places.

--- src/calculators/hpmsPDFMeasureAggregator.js
+++ src/calculators/hpmsPDFMeasureAggregator.js
@@ -1,4 +1,5 @@
 const CalculatePHED = require('./phed');
+const CalculateFreeFlow = require('./freeflow');
 const CalculateTTR = require('./ttr');
 
 const precisionRound = (number, precision = 0) => {
@@ -48,8 +49,19 @@ function hpmsPDFMeasureAggregator({ YEAR, TIME, MEAN }) {
       }
     } = CalculateTTR(tmcAttrs, tmcFiveteenMinIndex, MEAN, 'hpms');
 
+    const freeflow = CalculateFreeFlow(tmcAttrs, tmcFiveteenMinIndex);
+    const { freeflowTT } = freeflow;
+
+    // tmcAttributes,
+    // freeflowTT,
+    // tmcFiveteenMinIndex,
+    // trafficDistribution,
+    // time = 12,
+    // mean = DEFAULT_MEAN_TYPE,
+    // colMappings = 'avail'
     const { DIR_AADT, OCC_FAC, PHED } = CalculatePHED(
       tmcAttrs,
+      freeflowTT,
       tmcFiveteenMinIndex,
       trafficDistribution,
       TIME,
@@ -68,39 +80,39 @@ function hpmsPDFMeasureAggregator({ YEAR, TIME, MEAN }) {
       Segment_Length: precisionRound(tmcAttrs.length, 3),
       Directionality: tmcAttrs.direction || '',
 
-      DIR_AADT: precisionRound(DIR_AADT),
+      DIR_AADT: precisionRound(DIR_AADT, 2),
 
       LOTTR_AMP: precisionRound(LOTTR_AMP, 2),
-      TT_AMP50PCT: precisionRound(TT_AMP50PCT),
-      TT_AMP80PCT: precisionRound(TT_AMP80PCT),
+      TT_AMP50PCT: precisionRound(TT_AMP50PCT, 2),
+      TT_AMP80PCT: precisionRound(TT_AMP80PCT, 2),
       LOTTR_MIDD: precisionRound(LOTTR_MIDD, 2),
-      TT_MIDD50PCT: precisionRound(TT_MIDD50PCT),
-      TT_MIDD80PCT: precisionRound(TT_MIDD80PCT),
+      TT_MIDD50PCT: precisionRound(TT_MIDD50PCT, 2),
+      TT_MIDD80PCT: precisionRound(TT_MIDD80PCT, 2),
       LOTTR_PMP: precisionRound(LOTTR_PMP, 2),
-      TT_PMP50PCT: precisionRound(TT_PMP50PCT),
-      TT_PMP80PCT: precisionRound(TT_PMP80PCT),
+      TT_PMP50PCT: precisionRound(TT_PMP50PCT, 2),
+      TT_PMP80PCT: precisionRound(TT_PMP80PCT, 2),
       LOTTR_WE: precisionRound(LOTTR_WE, 2),
-      TT_WE50PCT: precisionRound(TT_WE50PCT),
-      TT_WE80PCT: precisionRound(TT_WE80PCT),
+      TT_WE50PCT: precisionRound(TT_WE50PCT, 2),
+      TT_WE80PCT: precisionRound(TT_WE80PCT, 2),
 
       TTTR_AMP: precisionRound(TTTR_AMP, 2),
-      TTT_AMP50PCT: precisionRound(TTT_AMP50PCT),
-      TTT_AMP95PCT: precisionRound(TTT_AMP95PCT),
+      TTT_AMP50PCT: precisionRound(TTT_AMP50PCT, 2),
+      TTT_AMP95PCT: precisionRound(TTT_AMP95PCT, 2),
       TTTR_MIDD: precisionRound(TTTR_MIDD, 2),
-      TTT_MIDD50PCT: precisionRound(TTT_MIDD50PCT),
-      TTT_MIDD95PCT: precisionRound(TTT_MIDD95PCT),
+      TTT_MIDD50PCT: precisionRound(TTT_MIDD50PCT, 2),
+      TTT_MIDD95PCT: precisionRound(TTT_MIDD95PCT, 2),
       TTTR_PMP: precisionRound(TTTR_PMP, 2),
-      TTT_PMP50PCT: precisionRound(TTT_PMP50PCT),
-      TTT_PMP95PCT: precisionRound(TTT_PMP95PCT),
+      TTT_PMP50PCT: precisionRound(TTT_PMP50PCT, 2),
+      TTT_PMP95PCT: precisionRound(TTT_PMP95PCT, 2),
       TTTR_OVN: precisionRound(TTTR_OVN, 2),
-      TTT_OVN50PCT: precisionRound(TTT_OVN50PCT),
-      TTT_OVN95PCT: precisionRound(TTT_OVN95PCT),
+      TTT_OVN50PCT: precisionRound(TTT_OVN50PCT, 2),
+      TTT_OVN95PCT: precisionRound(TTT_OVN95PCT, 2),
       TTTR_WE: precisionRound(TTTR_WE, 2),
-      TTT_WE50PCT: precisionRound(TTT_WE50PCT),
-      TTT_WE95PCT: precisionRound(TTT_WE95PCT),
+      TTT_WE50PCT: precisionRound(TTT_WE50PCT, 2),
+      TTT_WE95PCT: precisionRound(TTT_WE95PCT, 2),
 
       PHED: precisionRound(PHED, 3),
-      OCC_FAC: precisionRound(OCC_FAC, 1),
+      OCC_FAC: precisionRound(OCC_FAC, 2),
 
       METRIC_SOURCE: 1, // 'NPMRDS'
       COMMENTS: ''
