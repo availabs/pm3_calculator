c296ecb4c054ce6119835e7bbb560f9400e652a7
Paul Tomchik <pauljtomchik@live.com>
2018-09-26 21:56:37 -0400

    Added new ATRI based measures.

--- src/calculators/hpmsPDFMeasureAggregator.js
+++ src/calculators/hpmsPDFMeasureAggregator.js
@@ -12,8 +12,27 @@ const precisionRound = (number, precision = 0) => {
   return Math.round(+number * factor) / factor;
 };
 
+const directionCodeMappings = {
+  N: 1,
+  S: 2,
+  E: 3,
+  W: 4
+};
+
 function hpmsPDFMeasureAggregator({ YEAR, TIME, MEAN }) {
   return (tmcAttrs, trafficDistribution, tmcFiveteenMinIndex) => {
+    const {
+      direction,
+      f_system,
+      faciltype,
+      nhs,
+      nhs_pct,
+      length,
+      state_code,
+      tmc,
+      ua_code
+    } = tmcAttrs;
+
     const {
       lottr: {
         LOTTR_AMP,
@@ -69,16 +88,18 @@ function hpmsPDFMeasureAggregator({ YEAR, TIME, MEAN }) {
       'hpms'
     );
 
+    const nhsRatio = nhs_pct < 100 ? nhs_pct / 100 : 1
+
     return {
       Year_Record: YEAR || '',
-      State_Code: +tmcAttrs.state_code || '',
-      Travel_Time_Code: tmcAttrs.tmc || '',
-      F_System: +tmcAttrs.f_system || '',
-      Urban_Code: +tmcAttrs.ua_code || '',
-      Facility_Type: +tmcAttrs.faciltype || '',
-      NHS: +tmcAttrs.nhs || '',
-      Segment_Length: precisionRound(tmcAttrs.length, 3),
-      Directionality: tmcAttrs.direction || '',
+      State_Code: +state_code || '',
+      Travel_Time_Code: tmc || '',
+      F_System: +f_system || '',
+      Urban_Code: +ua_code || '',
+      Facility_Type: +faciltype || '',
+      NHS: +nhs || '',
+      Segment_Length: precisionRound(length * nhsRatio, 3),
+      Directionality: directionCodeMappings[direction] || 5,
 
       DIR_AADT: precisionRound(DIR_AADT, 2),
 
@@ -111,7 +132,7 @@ function hpmsPDFMeasureAggregator({ YEAR, TIME, MEAN }) {
       TTT_WE50PCT: precisionRound(TTT_WE50PCT, 2),
       TTT_WE95PCT: precisionRound(TTT_WE95PCT, 2),
 
-      PHED: precisionRound(PHED, 3),
+      PHED: precisionRound(PHED * nhsRatio, 3),
       OCC_FAC: precisionRound(OCC_FAC, 2),
 
       METRIC_SOURCE: 1, // 'NPMRDS'
