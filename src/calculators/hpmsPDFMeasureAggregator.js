382642b595061028a7a82a71de501bd5a26e08e6
Paul Tomchik <pauljtomchik@gmail.com>
2019-05-07 22:18:29 -0400

    Fixed bug in hpmsPDFMeasureAggregator where falsey values changed to empty strings.

--- src/calculators/hpmsPDFMeasureAggregator.js
+++ src/calculators/hpmsPDFMeasureAggregator.js
@@ -1,3 +1,5 @@
+const { isNil, isNaN } = require('lodash');
+
 const CalculatePHED = require('./phed');
 const CalculateFreeFlow = require('./freeflow');
 const CalculateTTR = require('./ttr');
@@ -18,6 +20,8 @@ const directionCodeMappings = {
   E: 3,
   W: 4
 };
+
+const numOrNothing = v => (isNil(v) || isNaN(+v) ? '' : +v);
 
 function hpmsPDFMeasureAggregator({ YEAR, TIME, MEAN }) {
   return (tmcAttrs, trafficDistribution, tmcFiveteenMinIndex) => {
@@ -88,52 +92,52 @@ function hpmsPDFMeasureAggregator({ YEAR, TIME, MEAN }) {
       'hpms'
     );
 
-    const nhsRatio = nhs_pct < 100 ? nhs_pct / 100 : 1
+    const nhsRatio = nhs_pct < 100 ? nhs_pct / 100 : 1;
 
     return {
-      Year_Record: YEAR || '',
-      State_Code: +state_code || '',
+      Year_Record: numOrNothing(YEAR),
+      State_Code: numOrNothing(state_code),
       Travel_Time_Code: tmc || '',
-      F_System: +f_system || '',
-      Urban_Code: +ua_code || '',
-      Facility_Type: +faciltype || '',
-      NHS: +nhs || '',
-      Segment_Length: precisionRound(length * nhsRatio, 3),
-      Directionality: directionCodeMappings[direction] || 5,
+      F_System: numOrNothing(f_system),
+      Urban_Code: numOrNothing(ua_code),
+      Facility_Type: numOrNothing(faciltype),
+      NHS: numOrNothing(nhs),
+      Segment_Length: numOrNothing(precisionRound(length * nhsRatio, 3)),
+      Directionality: numOrNothing(directionCodeMappings[direction] || 5),
 
-      DIR_AADT: precisionRound(DIR_AADT, 2),
+      DIR_AADT: numOrNothing(precisionRound(DIR_AADT, 2)),
 
-      LOTTR_AMP: precisionRound(LOTTR_AMP, 2),
-      TT_AMP50PCT: precisionRound(TT_AMP50PCT, 2),
-      TT_AMP80PCT: precisionRound(TT_AMP80PCT, 2),
-      LOTTR_MIDD: precisionRound(LOTTR_MIDD, 2),
-      TT_MIDD50PCT: precisionRound(TT_MIDD50PCT, 2),
-      TT_MIDD80PCT: precisionRound(TT_MIDD80PCT, 2),
-      LOTTR_PMP: precisionRound(LOTTR_PMP, 2),
-      TT_PMP50PCT: precisionRound(TT_PMP50PCT, 2),
-      TT_PMP80PCT: precisionRound(TT_PMP80PCT, 2),
-      LOTTR_WE: precisionRound(LOTTR_WE, 2),
-      TT_WE50PCT: precisionRound(TT_WE50PCT, 2),
-      TT_WE80PCT: precisionRound(TT_WE80PCT, 2),
+      LOTTR_AMP: numOrNothing(precisionRound(LOTTR_AMP, 2)),
+      TT_AMP50PCT: numOrNothing(precisionRound(TT_AMP50PCT, 2)),
+      TT_AMP80PCT: numOrNothing(precisionRound(TT_AMP80PCT, 2)),
+      LOTTR_MIDD: numOrNothing(precisionRound(LOTTR_MIDD, 2)),
+      TT_MIDD50PCT: numOrNothing(precisionRound(TT_MIDD50PCT, 2)),
+      TT_MIDD80PCT: numOrNothing(precisionRound(TT_MIDD80PCT, 2)),
+      LOTTR_PMP: numOrNothing(precisionRound(LOTTR_PMP, 2)),
+      TT_PMP50PCT: numOrNothing(precisionRound(TT_PMP50PCT, 2)),
+      TT_PMP80PCT: numOrNothing(precisionRound(TT_PMP80PCT, 2)),
+      LOTTR_WE: numOrNothing(precisionRound(LOTTR_WE, 2)),
+      TT_WE50PCT: numOrNothing(precisionRound(TT_WE50PCT, 2)),
+      TT_WE80PCT: numOrNothing(precisionRound(TT_WE80PCT, 2)),
 
-      TTTR_AMP: precisionRound(TTTR_AMP, 2),
-      TTT_AMP50PCT: precisionRound(TTT_AMP50PCT, 2),
-      TTT_AMP95PCT: precisionRound(TTT_AMP95PCT, 2),
-      TTTR_MIDD: precisionRound(TTTR_MIDD, 2),
-      TTT_MIDD50PCT: precisionRound(TTT_MIDD50PCT, 2),
-      TTT_MIDD95PCT: precisionRound(TTT_MIDD95PCT, 2),
-      TTTR_PMP: precisionRound(TTTR_PMP, 2),
-      TTT_PMP50PCT: precisionRound(TTT_PMP50PCT, 2),
-      TTT_PMP95PCT: precisionRound(TTT_PMP95PCT, 2),
-      TTTR_OVN: precisionRound(TTTR_OVN, 2),
-      TTT_OVN50PCT: precisionRound(TTT_OVN50PCT, 2),
-      TTT_OVN95PCT: precisionRound(TTT_OVN95PCT, 2),
-      TTTR_WE: precisionRound(TTTR_WE, 2),
-      TTT_WE50PCT: precisionRound(TTT_WE50PCT, 2),
-      TTT_WE95PCT: precisionRound(TTT_WE95PCT, 2),
+      TTTR_AMP: numOrNothing(precisionRound(TTTR_AMP, 2)),
+      TTT_AMP50PCT: numOrNothing(precisionRound(TTT_AMP50PCT, 2)),
+      TTT_AMP95PCT: numOrNothing(precisionRound(TTT_AMP95PCT, 2)),
+      TTTR_MIDD: numOrNothing(precisionRound(TTTR_MIDD, 2)),
+      TTT_MIDD50PCT: numOrNothing(precisionRound(TTT_MIDD50PCT, 2)),
+      TTT_MIDD95PCT: numOrNothing(precisionRound(TTT_MIDD95PCT, 2)),
+      TTTR_PMP: numOrNothing(precisionRound(TTTR_PMP, 2)),
+      TTT_PMP50PCT: numOrNothing(precisionRound(TTT_PMP50PCT, 2)),
+      TTT_PMP95PCT: numOrNothing(precisionRound(TTT_PMP95PCT, 2)),
+      TTTR_OVN: numOrNothing(precisionRound(TTTR_OVN, 2)),
+      TTT_OVN50PCT: numOrNothing(precisionRound(TTT_OVN50PCT, 2)),
+      TTT_OVN95PCT: numOrNothing(precisionRound(TTT_OVN95PCT, 2)),
+      TTTR_WE: numOrNothing(precisionRound(TTTR_WE, 2)),
+      TTT_WE50PCT: numOrNothing(precisionRound(TTT_WE50PCT, 2)),
+      TTT_WE95PCT: numOrNothing(precisionRound(TTT_WE95PCT, 2)),
 
-      PHED: precisionRound(PHED * nhsRatio, 3),
-      OCC_FAC: precisionRound(OCC_FAC, 2),
+      PHED: numOrNothing(precisionRound(PHED * nhsRatio, 3)),
+      OCC_FAC: numOrNothing(precisionRound(OCC_FAC, 2)),
 
       METRIC_SOURCE: 1, // 'NPMRDS'
       COMMENTS: ''
